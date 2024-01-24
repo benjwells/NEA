9655a639fe6baf7ac868244c7d0c195aa22e5be2
benjwells <153030129+benjwells@users.noreply.github.com>
2024-01-24 13:27:23 +0000

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -3,37 +3,32 @@ from login_page import Button
 
 pygame.init()
 
-s_width, s_height = 1920, 1080 
+s = pygame.display.set_mode((1920,1080))
 
-screen = pygame.display.set_mode((s_width, s_height))
+bg = pygame.image.load("images/modeSelection.png")
+bg = pygame.transform.scale(bg, (1920, 1080))
 
-bg_image = "images/modeSelection.png"
-bg = pygame.image.load(bg_image)
-bg = pygame.transform.scale(bg, (s_width, s_height))
+game_b = pygame.image.load("images/InteractiveGameButton.png")
+simulation_b = pygame.image.load("images/SimulationButton.png")
+statistics_b = pygame.image.load("images/StatisticsButton.png")
+options_b = pygame.image.load("images/options.png")
 
-interactive_game_image = pygame.image.load("images/InteractiveGameButton.png")
-simulation_image = pygame.image.load("images/SimulationButton.png")
-statistics_image = pygame.image.load("images/StatisticsButton.png")
-options_image = pygame.image.load("images/options.png")
+game = Button(760, 300, game_b, 400, 100)
+simulation = Button(760, 425, simulation_b, 400, 100)
+stats = Button(760, 550, statistics_b, 400, 100)
+optn = Button(760, 675, options_b, 400, 100)
 
-button_width, button_height = 400, 100  
-spacing = 30  
-game_button = Button(s_width/2 - button_width/2, s_height/2 - 2*button_height - 1.5*spacing, interactive_game_image, button_width, button_height)
-simulation_button = Button(s_width/2 - button_width/2, s_height/2 - button_height - 0.5*spacing, simulation_image, button_width, button_height)
-statistics_button = Button(s_width/2 - button_width/2, s_height/2 + 0.5*spacing, statistics_image, button_width, button_height)
-options_button = Button(s_width/2 - button_width/2, s_height/2 + button_height + 1.5*spacing, options_image, button_width, button_height)
 
 font = pygame.font.Font(None, 50)
-
 loading_text = font.render("Loading...", True, pygame.Color('white'))
 
-screen.blit(loading_text, (s_width/2 - loading_text.get_width()/2, s_height/2 - loading_text.get_height()/2))
+s.blit(loading_text, (960 - loading_text.get_width()/2, 540 - loading_text.get_height()/2))
 
-b_width, b_height = 500, 50
-bord = pygame.Rect(s_width/2 - b_width/2, s_height/2 + loading_text.get_height(), b_width, b_height)
-filled_rect = pygame.Rect(s_width/2 - b_width/2, s_height/2 + loading_text.get_height(), 0, b_height)
 
+bord = pygame.Rect(710, 540 + loading_text.get_height(), 500, 50)
+filled_rect = pygame.Rect(710, 540 + loading_text.get_height(), 0, 50)
 loading = True
+
 ticks = pygame.time.get_ticks()
 while loading:
   for event in pygame.event.get():
@@ -42,12 +37,12 @@ while loading:
 
   if (pygame.time.get_ticks() - ticks) < 3000:
     
-    filled_rect.width = ((pygame.time.get_ticks() - ticks) / 3000) * b_width
+    filled_rect.width = ((pygame.time.get_ticks() - ticks) / 3000) * 500
   else:
     loading = False
 
-  pygame.draw.rect(screen, pygame.Color('white'), bord, 2)
-  pygame.draw.rect(screen, pygame.Color('white'), filled_rect)
+  pygame.draw.rect(s, pygame.Color('white'), bord, 2)
+  pygame.draw.rect(s, pygame.Color('white'), filled_rect)
 
   pygame.display.flip()
 
@@ -57,30 +52,22 @@ while running:
     if event.type == pygame.QUIT:
       running = False
     if event.type == pygame.MOUSEBUTTONDOWN:
-      if game_button.isOver(pygame.mouse.get_pos()):
-        
-        bg_image = "images/InGameScreen.png"
-        bg = pygame.image.load(bg_image)
-        bg = pygame.transform.scale(bg, (s_width, s_height))
-
-        screen.blit(bg, (0, 0))
-        pygame.display.flip()
-  
+      if game.Over(pygame.mouse.get_pos()):
         exec(open("interactive_game.py").read())
-      elif simulation_button.isOver(pygame.mouse.get_pos()):
+      elif simulation.Over(pygame.mouse.get_pos()):
         exec(open("simulation.py").read())
         
-      elif statistics_button.isOver(pygame.mouse.get_pos()):
+      elif stats.Over(pygame.mouse.get_pos()):
         exec(open("statistics.py").read())
         
-      elif options_button.isOver(pygame.mouse.get_pos()):
+      elif optn.Over(pygame.mouse.get_pos()):
         exec(open("options.py").read())
         
-  screen.blit(bg, (0, 0))
-  game_button.draw(screen)
-  simulation_button.draw(screen)
-  statistics_button.draw(screen)
-  options_button.draw(screen)
+  s.blit(bg, (0, 0))
+  game.draw(s)
+  simulation.draw(s)
+  stats.draw(s)
+  optn.draw(s)
   
   pygame.display.flip()
   
