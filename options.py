2dc1c6bbece976a1f97de4ccad9bd8ce41219023
benjwells <153030129+benjwells@users.noreply.github.com>
2024-01-31 13:09:03 +0000

    Update options.py

--- options.py
+++ options.py
@@ -2,10 +2,44 @@ import pygame
 
 pygame.init()
 
-s = pygame.display.set_mode((1920,1080))
+s_width = 1920
+s_height = 1080
+s = pygame.display.set_mode((s_width, s_height))
 
 bg = pygame.image.load("images/optionsBg.png")
 bg = pygame.transform.scale(bg, (1920, 1080))
+                            
+font = pygame.font.Font(None, 50)
+loading_text = font.render("Loading...", True, pygame.Color('white'))
+
+s.blit(loading_text, (s_width/2 - loading_text.get_width()/2,
+s_height/2 - loading_text.get_height()/2))
+
+
+b_width, b_height = 500, 50
+rect_top = s_height/2 + loading_text.get_height()
+outline_rect = filled_rect = pygame.Rect(s_width/2 - b_width/2, rect_top, b_width, b_height)
+filled_rect.width = 0
+pygame.draw.rect(s, pygame.Color('white'), outline_rect)
+
+loading = True
+ticks = pygame.time.get_ticks()
+while loading:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+
+  if (pygame.time.get_ticks() - ticks) < 3000:
+    filled_rect.width = ((pygame.time.get_ticks() - ticks) / 3000) * b_width
+  else:
+    loading = False
+
+  pygame.draw.rect(s, pygame.Color('white'), outline_rect, 2)
+  pygame.draw.rect(s, pygame.Color('white'), filled_rect)
+
+  pygame.display.flip()
+  
+
 
 class Slider:
     def __init__(self, x, y, w, h, min_val, max_val, color):
@@ -163,7 +197,7 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if exit_button.is_clicked(event.pos):
               exec(open("menu.py").read())
-        elif event.type == pygame.MOUSEBUTTONUP:  
+        elif event.type == pygame.MOUSEBUTTONUP:  # Handle mouse button release events
           if mute_m.handle_event(event):
              if mute_m.clicked:
                music_vol = 0
