1744e643a61d1024f5dc8bf7645fc09a0a4e1f7c
benjwells <153030129+benjwells@users.noreply.github.com>
2024-01-31 13:12:38 +0000

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -1,7 +1,16 @@
 import pygame
 import sqlite3
 from sqlite3 import Error
-import hashlib,os
+import hashlib
+
+try:
+  from pygame import mixer
+  mixer.init() 
+  mixer.music.load("bg.mp3") 
+  mixer.music.set_volume(0.7) 
+  mixer.music.play()
+except Exception as e:
+  print(e)
 
 pygame.init()
 
@@ -13,7 +22,6 @@ bg = pygame.transform.scale(bg, (1920, 1080))
 input_boxes = [pygame.Rect(230, 500 + i*120 - 120, 500, 80) for i in range(3)]
 colors = [pygame.Color('white') for _ in range(3)]
 active_box = None
-
 
 font = pygame.font.Font(r"fonts/Dungeon Depths.otf", 25) 
 global texts
@@ -35,8 +43,8 @@ class Button:
     self.width = width
     self.height = height
     self.image = pygame.transform.scale(self.image, (self.width, self.height))
-    self.rect = self.image.get_rect()
-    self.rect.topleft = (x, y)
+    self.rect = self.image.get_rect()  # Create a rect style object using the get_rect method of the image
+    self.rect.topleft = (x, y) 
 
   def draw(self, surface):
     surface.blit(self.image, self.rect)
@@ -56,7 +64,7 @@ login_button = Button(1150, 600, login_image, 500, 120)
 
 class Database:
   def __init__(self, db_name):
-      self.conn = self.create_connection(db_name) #connects to ther database
+      self.conn = self.create_connection(db_name)
       if self.conn is not None:
           self.create_table()
 
@@ -64,79 +72,79 @@ class Database:
   def create_connection(self, db_name):
       conn = None
       try:
-          conn = sqlite3.connect(db_name)  
-          return conn
+        conn = sqlite3.connect(db_name)  
+        return conn
       except Error as e:
-          print(e)
+        print(e)
       return conn
 
-  def create_table(self): # creates the table with the new values to be stored
-      try:
-          sql = '''CREATE TABLE accounts (
-              username TEXT NOT NULL,
-              password TEXT NOT NULL,
-              total_tries INTEGER DEFAULT 0, #new value 1
-              successful_tries INTEGER DEFAULT 0, #new value 2
-              success_rate REAL DEFAULT 0.0 #new value 3
-          );'''
-          self.conn.execute(sql)
-      except Error as e:
-          print(e)
-        
-  def increment_tries(self, success): #a function that allows the values to be increased accordingly
+  def create_table(self):
+    try:
+        sql = '''CREATE TABLE accounts (
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            total_tries INTEGER DEFAULT 0,
+            successful_tries INTEGER DEFAULT 0,
+            success_rate REAL DEFAULT 0.0
+        );'''
+        self.conn.execute(sql)
+    except Error as e:
+      print(e)
+
+  def increment_tries(self, success):
     sql = '''SELECT * FROM accounts WHERE username = ?;'''
     cur = self.conn.cursor()
-    cur.execute(sql, texts[0])
+    cur.execute(sql, (texts[0],))
     record = cur.fetchall()
     temp = record[0]
     record = []
     for i in temp:
-        record.append(i)
- 
+      record.append(i)
+  
     record[2] +=1
     if success:
-        record[3] +=1
+      record[3] +=1
     record[4] = round((record[3]/record[2])*100)
     sql = '''UPDATE accounts SET total_tries = ?,successful_tries = ?, success_rate = ? WHERE username = ?;'''
     self.conn.execute(sql, (record[2],record[3],record[4],texts[0]))
     self.conn.commit()
 
-  def login(self, username, password): 
-      if self.check_account_exists((username, password)): #checks if the account exists
-          print(f"Welcome {username}!") #used to check whether the function was working when experiencing issues
-      else:
-          print("Invalid username or password!")
+  def login(self, username, password):
+    if self.check_account_exists((username, password)):
+      print(f"Welcome {username}!")
+    else:
+      print("Invalid username or password!")
 
-  def create_account(self, account): #function that creates a new account
-      username, password = account
-      print(password)
-      hashed_password = hash_password(password)
-      sql = '''INSERT INTO accounts(username, password,total_tries,successful_tries,success_rate)
-            VALUES(?, ?,0,0,0);'''
-      self.conn.execute(sql, (username, hashed_password))
-      self.conn.commit()
+  def create_account(self, account):
+    username, password = account
+    print(password)
+    hashed_password = hash_password(password)
+    sql = '''INSERT INTO accounts(username, password,total_tries,successful_tries,success_rate)
+          VALUES(?, ?,0,0,0);'''
+    self.conn.execute(sql, (username, hashed_password))
+    self.conn.commit()
 
-  def check_username_exists(self, username): #function that checks if the username exists
-      sql = '''SELECT * FROM accounts WHERE username = ?'''
-      cur = self.conn.cursor()
-      cur.execute(sql, (username,))
-      rows = cur.fetchall()
-      return len(rows) > 0
+  def check_username_exists(self, username):
+    sql = '''SELECT * FROM accounts WHERE username = ?'''
+    cur = self.conn.cursor()
+    cur.execute(sql, (username,))
+    rows = cur.fetchall()
+    return len(rows) > 0
 
-  def check_account_exists(self, account): #checks whether the account exists and if a new one can be created
-      username, password = account
-      hashed_password = Database.hash_password(password)
-      sql = '''SELECT * FROM accounts WHERE username = ? AND password = ?;'''
-      cur = self.conn.cursor()
-      cur.execute(sql, (username, hashed_password))
-      rows = cur.fetchall()
-      return len(rows) > 0
+  def check_account_exists(self, account):
+    username, password = account
+    hashed_password = Database.hash_password(password)
+    sql = '''SELECT * FROM accounts WHERE username = ? AND password = ?;'''
+    cur = self.conn.cursor()
+    cur.execute(sql, (username, hashed_password))
+    rows = cur.fetchall()
+    return len(rows) > 0
 
-  def hash_password(password): #hashes the password
+  def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 
-account = Database('user_accounts.db') # creates the database
+account = Database('user_accounts.db')
 
 
 
@@ -146,67 +154,67 @@ error_color = pygame.Color('red')
 
 while True:
   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          pygame.quit()
-      if event.type == pygame.MOUSEBUTTONDOWN:
-          for i, box in enumerate(input_boxes):
-              display_text = texts[i]
-              if i > 0 and active_box != i and passwords[i - 1] == '':
-                display_text = ['Password', 'Confirm Password'][i - 1]
-              txt_surface = font.render(display_text, True, pygame.Color('white'))
-              screen.blit(txt_surface, (box.x+5, box.y+5))
-              pygame.draw.rect(screen, colors[i], box, 2)
-              if box.collidepoint(event.pos):
-                  active_box = i
-                  colors[i] = pygame.Color('darkgreen')
-                  if texts[i] in ['Username', 'Password', 'Confirm Password']:
-                      texts[i] = ''
-              else:
-                  colors[i] = pygame.Color('white')
-                  if texts[i] == '':
-                      texts[i] = ['Username', 'Password', 'Confirm Password'][i]
-          if create_account_button.Over(pygame.mouse.get_pos()):
-              if texts[0] == 'Username' or passwords[0] == '':
-                  error_message = "Please enter a valid username or password!"
-                  error_color = pygame.Color('red')
-              elif passwords[0] != passwords[1]:
-                  error_message = "Passwords do not match!"
-                  error_color = pygame.Color('red')
-              elif not account.check_username_exists(texts[0]):
-                  account.create_account([texts[0], passwords[0]])
-                  error_message = "Account created successfully!"
-                  error_color = pygame.Color('green')
-              else:
-                  error_message = "Username already exists!"
-                  error_color = pygame.Color('red')
+    if event.type == pygame.QUIT:
+      pygame.quit()
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        for i, box in enumerate(input_boxes):
+            display_text = texts[i]
+            if i > 0 and active_box != i and passwords[i - 1] == '':
+              display_text = ['Password', 'Confirm Password'][i - 1]
+            txt_surface = font.render(display_text, True, pygame.Color('white'))
+            screen.blit(txt_surface, (box.x+5, box.y+5))
+            pygame.draw.rect(screen, colors[i], box, 2)
+            if box.collidepoint(event.pos):
+              active_box = i
+              colors[i] = pygame.Color('darkgreen')
+              if texts[i] in ['Username', 'Password', 'Confirm Password']:
+                texts[i] = ''
+            else:
+              colors[i] = pygame.Color('white')
+              if texts[i] == '':
+                texts[i] = ['Username', 'Password', 'Confirm Password'][i]
+        if create_account_button.Over(pygame.mouse.get_pos()):
+            if texts[0] == 'Username' or passwords[0] == '':
+              error_message = "Please enter a valid username or password!"
+              error_color = pygame.Color('red')
+            elif passwords[0] != passwords[1]:
+              error_message = "Passwords do not match!"
+              error_color = pygame.Color('red')
+            elif not account.check_username_exists(texts[0]):
+              account.create_account([texts[0], passwords[0]])
+              error_message = "Account created successfully!"
+              error_color = pygame.Color('green')
+            else:
+              error_message = "Username already exists!"
+              error_color = pygame.Color('red')
 
-          if login_button.Over(pygame.mouse.get_pos()):
-              pygame.time.wait(300)
-              if texts[0] == 'Username' or passwords[0] == '':
-                  error_message = "Please enter a valid username or password!"
-                  error_color = pygame.Color('red')
-              elif not account.check_account_exists((texts[0], passwords[0])):
-                  error_message = "Sign up first!"
-                  error_color = pygame.Color('red')
-              elif passwords[0] != passwords[1]:
-                  error_message = "Passwords do not match!"
-                  error_color = pygame.Color('red')
-              else:
-                exec(open("menu.py").read())
+        if login_button.Over(pygame.mouse.get_pos()):
+          pygame.time.wait(300)
+          if texts[0] == 'Username' or passwords[0] == '':
+            error_message = "Please enter a valid username or password!"
+            error_color = pygame.Color('red')
+          elif not account.check_account_exists((texts[0], passwords[0])):
+            error_message = "Sign up first!"
+            error_color = pygame.Color('red')
+          elif passwords[0] != passwords[1]:
+            error_message = "Passwords do not match!"
+            error_color = pygame.Color('red')
+          else:
+            exec(open("menu.py").read())
                   
                   
-      if event.type == pygame.KEYDOWN:
-          if active_box is not None:
-              if event.key == pygame.K_BACKSPACE:
-                  texts[active_box] = texts[active_box][:-1]
-                  if active_box > 0:
-                      passwords[active_box - 1] = passwords[active_box - 1][:-1]
-              else:
-                  if active_box > 0:
-                      passwords[active_box - 1] += event.unicode
-                      texts[active_box] += '*'
-                  else:
-                      texts[active_box] += event.unicode
+    if event.type == pygame.KEYDOWN:
+      if active_box is not None:
+        if event.key == pygame.K_BACKSPACE:
+          texts[active_box] = texts[active_box][:-1]
+          if active_box > 0:
+            passwords[active_box - 1] = passwords[active_box - 1][:-1]
+        else:
+          if active_box > 0:
+            passwords[active_box - 1] += event.unicode
+            texts[active_box] += '*'
+          else:
+            texts[active_box] += event.unicode
 
   
   screen.fill((30, 30, 30))
